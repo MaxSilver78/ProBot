6eed7c6df624a1f33f37d75b74bd40f3e7467824
MaxSilver78 <53319427+MaxSilver78@users.noreply.github.com>
2019-07-26 22:41:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -226,8 +226,8 @@ message.channel.send(`**${message.author.username}, your :credit_card: balance i
 });
 
 client.on('message', async message => {
-    let amount = 7859558;
-    if(message.content.startsWith(prefix + "daily")) {
+    let amount = 17859558;
+    if(message.content.startsWith(prefix + "dddaillly")) {
     if(message.author.bot) return;
     if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
     
